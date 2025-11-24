b886f3863fbad8bfa942408de682e0d8f5cae054
baikseunghoo <100seounghoo@gmail.com>
2025-11-24 04:11:09 +0000

    임시저장

--- 3_소스코드/main.c
+++ 3_소스코드/main.c
@@ -1,40 +1,18 @@
 #include <stdio.h>
 
-/*
-    1. [설계]에서 정의한 변수, 배열, (필요하면) 구조체를 여기에 선언하세요.
-    
-    struct Student {
-        char name[50]     
-        int attend;
-        int totalClass;
-        int excused;
-        double rate;
-    };
-    
-    struct Student s;
-*/
-
-
-/*
-    2. [알고리즘]에서 설계한 핵심 기능 함수들을 여기에 선언하세요.
-*/
-
-#include <stdio.h>
-
 int main() {
 
-    // 이름, 출석일수, 총 수업일수, 인정 결석일수, 출석률을 저장
+    // 이름, 출석일수, 총 수업일수, 인정 결석일수, 출석률을 저장하는 구조체
     struct Student {
-        char name[50]     
-        int attend;
-        int totalClass;
-        int excused;
-        double rate;
+        char name[50];     // 학생 이름
+        int attend;        // 출석 일수
+        int totalClass;    // 총 수업 일수
+        int excused;       // 출석 인정 결석 일수
+        double rate;       // 출석률
     };
 
-    // 구조체 변수 선언(한 명의 학생 정보를 저장)
-    struct Student s;
-    int standard;  // 기준 출석률
+    struct Student s;       // 구조체 변수 선언
+    int standard;           // 기준 출석률
 
     printf("학생 이름을 입력하세요: ");
     scanf("%s", s.name);
@@ -50,7 +28,6 @@ int main() {
     printf("출석 인정 결석이 있습니까? (y/n): ");
     scanf(" %c", &answer);
 
-    // 조건에 따라 인정 결석 일수 입력
     if (answer == 'y' || answer == 'Y') {
         printf("출석 인정 결석 일수를 입력하세요: ");
         scanf("%d", &s.excused);
@@ -58,16 +35,14 @@ int main() {
         s.excused = 0;  // 인정 결석 없음
     }
 
-    // 출석률 계산
-    // (출석일수 + 인정결석일수) / 총 수업일수 × 100
-    s.rate = (s.attend + s.excused) / total * 100;
+    // 정상 계산식 (총 수업 일수는 s.totalClass)
+    s.rate = (double)(s.attend + s.excused) / s.totalClass * 100;
 
-    // 기준 출석률 입력
-    printf("기준 출석률을 입력하세요: ");
+    printf("기준 출석률을 입력하세요(퍼센트): ");
     scanf("%d", &standard);
 
-    // 출석률이 기준 이상인지 판정
-    if (s.rate = standard) {
+    // 정상 비교 연산자 (>= 기준 이상이면 이수)
+    if (s.rate >= standard) {
         printf("이수\n");
     } else {
         printf("미이수\n");
